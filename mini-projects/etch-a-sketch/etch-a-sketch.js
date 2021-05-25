b466fb24ece102227b2f2a7ba9218ae136080999
Jess <jrizzo.br@gmail.com>
2021-05-24 23:24:26 -0500

    Refactored code and finished core features

--- mini-projects/etch-a-sketch/etch-a-sketch.js
+++ mini-projects/etch-a-sketch/etch-a-sketch.js
@@ -1,17 +1,39 @@
-const container = document.querySelector('#grid-container');
+function createRows(gridSize=16){
 
-for (let i = 0; i < 16; i++){
-    let div_row = document.createElement('div');
-    div_row.className = 'row';
-    container.appendChild(div_row);
+    const container = document.querySelector('#grid-container');
+    for (let i = 0; i < gridSize; i++){
+        let div_row = document.createElement('div');
+        div_row.className = 'row';
+        container.appendChild(div_row);
+    }
 }
 
-const div_row = document.querySelectorAll('.row');
+function createCols(gridSize=16){
 
-div_row.forEach((item) => {
-    for (let j = 0; j < 16; j++){
-        let div_col = document.createElement('div');
-        div_col.className = 'col';
-        item.appendChild(div_col);
-    }
-});
+    const div_row = document.querySelectorAll('.row');
+    div_row.forEach((item) => {
+        for (let j = 0; j < gridSize; j++){
+            let div_col = document.createElement('div');
+            div_col.className = 'col';
+            item.appendChild(div_col);
+            item.addEventListener('mouseover', function(mouseOver){
+                mouseOver.target.style.backgroundColor = 'pink';
+            })
+        }
+    });
+}
+
+function clearGrid(){
+
+    const div_col = document.querySelectorAll('.col');
+    div_col.forEach((item) => {
+            item.style.backgroundColor = '';
+    });
+}
+        
+createRows();
+createCols();
+
+
+
+
