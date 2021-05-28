b2f68c2ca462489a8149bdc28fabcd3af2a75f8d
Jess <jrizzo.br@gmail.com>
2021-05-27 23:18:49 -0500

    Resolved tempConversion

---
+++ random-exercises/vanilla-js/javascript-exercises/tempConversion/tempConversion.js
@@ -0,0 +1,14 @@
+const ftoc = function(degree) {
+  return Math.round(((degree - 32) * 5/9) * 10) / 10;
+};
+
+const ctof = function(degree) {
+  return Math.round(((degree * 9/5) + 32) * 10) / 10;
+};
+
+module.exports = {
+  ftoc,
+  ctof
+};
+
+console.log(ftoc(32));
